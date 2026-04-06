ae4dfb6fde4d2062984f991790217bae484faebd
anindya-shbl <abhattacharya1@sastasundar.com>
2026-04-06 14:53:21 +0530

    giftcard button icon and new small function added

--- src/app/modules/dashboard/pay-now/pay-now.component.ts
+++ src/app/modules/dashboard/pay-now/pay-now.component.ts
@@ -225,8 +225,6 @@ export class PayNowComponent implements OnInit {
     this.trackOrderWebEngage()
 
   }
-
-
   orderDetailsWebEngage(dtls: any, items: any) {
     let webData = {
       'Order ID': dtls.OrderId.toString(),
@@ -282,22 +280,12 @@ export class PayNowComponent implements OnInit {
     }
     console.log('endpoint',endpoint)
     this.paynowService.getGatewayInfo(endpoint, fd).subscribe((response: any) => {
-      console.log(response);
-      // console.log(response['data']['data']['EasebuzzDetails']['PayUrl']);
       if (response && response['status'] == 200) {
         if(cfhFlag){
           this.msgText = 'Your payment is being processed through your Health Buddy Card balance. Please do not refresh or close the window.';
           this.spinner.hide();
-          
-          let orderInfoResponse = {
-            'orderDetails': this.orderDetails,
-            'paymentResponse': response['data']
-          };
-          fd.append('orderInfoResponse', JSON.stringify(orderInfoResponse));
-          this.orderService.cfhPayment('customercart/paynow_using_cfh', fd).subscribe((res: any) => {
-            console.log('CFH payment response', res);
-            this.orderService.redirectToSuccess();
-          })
+          this.CFHPayPayment(response);
+          this.spinner.hide();
         }
         else {
           this.msgText = 'Redirecting to payment gateway. Please do not refresh or close the window.';
@@ -305,19 +293,27 @@ export class PayNowComponent implements OnInit {
           this.EASEBUZZPepayment(EZdata);
           this.spinner.hide();
         }
-        
       }
     });
   }
-
+  CFHPayPayment(res: any) {
+    // console.log(res);
+    let fd = new FormData();
+    let orderInfoResponse = {
+      'orderDetails': this.orderDetails,
+      'paymentResponse': res['data']
+    };
+    fd.append('orderInfoResponse', JSON.stringify(orderInfoResponse));
+    this.orderService.cfhPayment('customercart/paynow_using_cfh', fd).subscribe((res: any) => {
+      console.log('CFH payment response', res);
+      this.orderService.redirectToSuccess();
+    })
+  }
   EASEBUZZPepayment(data: any) {
     // console.log(data);
     let url = data.PayUrl;
     window.open(url, '_self');
   }
-
-  
-
   
   getBalance() {
     this.isloading = true;
@@ -356,61 +352,5 @@ useCardToggle(){
     console.log(this.useCardBalance);
     console.log(this.onlyViaCfh);
 }
-  // getTransationList() {
-  //   // this.isloading = true;
-  //   // this.spinner.show();
-  //   let MobileNo = this.authService.Mobile;
-  //   this.orderService.getDetails(`webapi/cfh/trans_history?search=${MobileNo}&per_page=${this.pageSize}&page=${this.pageNo}`).subscribe((res: any) => {
-  //   if (res && res.status == 1) {
-  //     this.transationData = res.data;
-  //     if (res.data && res.data.data.length > 0) {
-  //       this.trnsHistory = [...this.trnsHistory, ...res.data.data];
-  //       if (this.pageNo < res.data.total_pages) {
-  //         this.loadMoreBtn = true;
-  //       } else {
-  //         this.loadMoreBtn = false;
-  //       }
-  //       this.isloading = false;
-  //       this.spinner.hide();
-  //     } else {
-  //       this.isloading = false;
-  //       this.spinner.hide();
-  //     }
-  //   } else {
-  //     this.isloading = false;
-  //     this.spinner.hide();
-  //   }
-  //   })
-  // }
-
-  // moreTransation() {
-  //   this.pageNo += 1;
-  //   this.isloading = true;
-  //   this.spinner.show();
-  //   this.getTransationList();
-  // }
-
-  // addCard() {
-  //   this.submitted = true;
-  //   if (this.AddCardForm.invalid) {
-  //     return;
-  //   } else {
-  //     let data = this.AddCardForm.value.code;
-  //     this.spinner.show();
-  //     this.orderService.getDetails(`webapi/cfh/assign_healthcard?HealthCardCode=${data}`).subscribe((res: any) => {
-  //       if (res && res.status == 1) {
-  //         this.toastr.success('Health Card Added Successfully');
-  //         this.AddCardForm.reset();
-  //         this.submitted = false;
-  //         this.getBalance();
-  //         // this.getTransationList();
-  //       } else {
-  //         this.spinner.hide();
-  //         this.toastr.error(res.data || 'Something went wrong');
-  //       }
-  //     })
-  //   }
-  // }
-
 
 }
