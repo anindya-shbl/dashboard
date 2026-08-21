dd33d51715a6c75120a213cc1312043e54e47f13
Anindya Bhattacharya <abhattacharya1@sastasundar.com>
2026-08-21 11:30:22 +0530

    js version update for staging only

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 // "deployUrl": "https://sastasundar.com/ngdashboard/V2_25/",
-// "deployUrl": "https://stage.sastasundar.com/ngdashboard/v_26.8.21_2/",
+// "deployUrl": "https://stage.sastasundar.com/ngdashboard/v_26.8.21_3/",
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
