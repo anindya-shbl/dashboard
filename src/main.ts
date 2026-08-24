31c20b4251920dd18e344ab232bfe77110184c1c
Anindya Bhattacharya <abhattacharya1@sastasundar.com>
2026-08-24 14:56:22 +0530

    js version update for live only

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-// "deployUrl": "https://sastasundar.com/ngdashboard/V2_35/",
+// "deployUrl": "https://sastasundar.com/ngdashboard/V2_36/",
 // "deployUrl": "https://stage.sastasundar.com/ngdashboard/",
 
 
